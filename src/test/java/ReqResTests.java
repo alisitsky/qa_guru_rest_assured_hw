0047ae6880504727ea2a9123dfbbc102591ff294
alisitsky <alisitsky@gmail.com>
2024-02-14 18:56:24 +0100

    init commit

---
+++ src/test/java/ReqResTests.java
@@ -0,0 +1,9 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class ReqResTests {
+
+}
